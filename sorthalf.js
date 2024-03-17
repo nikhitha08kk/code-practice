c00b2b30c20486e5c5ab7ede8cc4a2b054f90b8b
nikhitha08kk <nikhithakakkadan07@gmail.com>
2024-03-17 14:32:30 +0700

    first commit

---
+++ sorthalf.js
@@ -0,0 +1,15 @@
+function sortHalf(array){
+    const mid=Math.floor(array.length/2);
+    const firstHalf=array.slice(0,mid);
+    const secondHalf=array.slice(mid);
+    firstHalf.sort((p,q)=>p-q);
+    secondHalf.sort((p,q)=>q-p);
+    return firstHalf.concat(secondHalf);
+
+
+}
+const arr=[3,7,1,4,90,67,10,78,45];
+const sortedArray = sortHalf(arr);
+console.log(sortedArray);
+
+
